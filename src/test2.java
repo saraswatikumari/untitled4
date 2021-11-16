3ad91536fce49109b72528524e4cda40aa126990
saraswatikumari <saraswati.kumari_cs19@gla.ac.in>
2021-11-16 19:02:00 +0530

    Initial commit

---
+++ src/test2.java
@@ -0,0 +1,14 @@
+public class test2 {
+
+
+        public static void main(String args[]){
+            System.out.println( Integer.valueOf(20));
+            System.out.println( Integer .toBinaryString(20));
+            System.out.println(Integer.toOctalString(20));
+            System.out.println(Integer.toHexString(20));
+
+
+
+        }
+    }
+
